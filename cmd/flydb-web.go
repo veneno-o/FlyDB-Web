0fbc06d65376a3d4c7c06d1f57f0860af7cf6888
sjcsjc123 <1401189096@qq.com>
2023-07-24 00:06:22 +0800

    improve importance

--- cmd/flydb-web.go
+++ cmd/flydb-web.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	v1 "FlyDB-Web/pkg/cache"
 	"FlyDB-Web/pkg/middleware"
 	"FlyDB-Web/pkg/router"
 	"fmt"
+	"github.com/ByteStorage/FlyDB/structure"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -17,4 +22,23 @@ func main() {
 	if err != nil {
 		_ = fmt.Errorf("gin run error: %v", err)
 	}
+	//graceful shutdown
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGKILL)
+
+	<-quit
+	for _, user := range v1.GetUserStore().GetUsers() {
+		for _, db := range user.Db {
+			switch v := db.(type) {
+			case *structure.StringStructure:
+				err := v.Stop()
+				if err != nil {
+					fmt.Println(err)
+				}
+			default:
+				fmt.Println("unknown type")
+			}
+		}
+	}
+
 }
--- pkg/service/string.go
+++ pkg/service/string.go
@@ -1,8 +1,8 @@
 package service
 
 import (
+	v1 "FlyDB-Web/pkg/cache"
 	R "FlyDB-Web/pkg/result"
-	"github.com/ByteStorage/FlyDB/config"
 	"github.com/ByteStorage/FlyDB/structure"
 	"github.com/gin-gonic/gin"
 )
@@ -18,27 +18,28 @@ func PutString(c *gin.Context) {
 		R.Error(c, 400, "user not login")
 		return
 	}
-	defaultOptions := config.DefaultOptions
-	defaultOptions.DirPath = "/var/lib/flydb/" + id.(string)
-	dbs, err := structure.NewStringStructure(defaultOptions)
-	if err != nil {
-		R.Error(c, 500, err.Error())
-		return
+	store := v1.GetUserStore()
+	user := store.GetUser(id.(string))
+	for _, db := range user.Db {
+		if dbs, ok := db.(*structure.StringStructure); ok {
+			var req RequestBody
+			err := c.BindJSON(&req)
+			if err != nil {
+				R.Error(c, 400, err.Error())
+				return
+			}
+			err = dbs.Set(req.Key, req.Value, 0)
+			if err != nil {
+				R.Error(c, 500, err.Error())
+				return
+			}
+			R.Success(c, gin.H{
+				"key":   req.Key,
+				"value": req.Value,
+			})
+			return
+		}
 	}
-	var req RequestBody
-	err = c.BindJSON(&req)
-	if err != nil {
-		R.Error(c, 400, err.Error())
-		return
-	}
-	err = dbs.Set(req.Key, req.Value, 0)
-	if err != nil {
-		R.Error(c, 500, err.Error())
-		return
-	}
-	R.Success(c, gin.H{
-		"key":   req.Key,
-		"value": req.Value,
-	})
+	R.Error(c, 500, "unknown type")
 
 }
